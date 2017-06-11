a80579f4f758b09a0b1457d76669aa4fa4bd18a9
Arsalan khan <arsalan.ak777@gmail.com>
2017-06-12 00:18:20 +0500

    Initial commit

---
+++ app/src/main/java/com/example/arsalankhan/blogapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.arsalankhan.blogapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    // for menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId()==R.id.addpost){
+            startActivity(new Intent(this,postActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
