5c4dabf4de894bc35a7150a21e60c63cd9c161df
Arsalan khan <arsalan.ak777@gmail.com>
2017-06-17 18:03:19 +0500

    initial commit

--- app/src/main/java/com/example/arsalankhan/blogapp/LoginUser.java
+++ app/src/main/java/com/example/arsalankhan/blogapp/LoginUser.java
@@ -6,14 +6,25 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +38,12 @@ public class LoginUser extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabaseUser;
     private ProgressDialog progressDialog;
+    private SignInButton mGoogleBtn;
+
+    private static final int RC_SIGN_IN=1;
+    private GoogleApiClient mGoogleApiClient;
+
+    private static final String TAG="LoginUser";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,16 +52,95 @@ public class LoginUser extends AppCompatActivity {
         editTextUserName= (EditText) findViewById(R.id.LoginUserNameField);
         editTextPassword= (EditText) findViewById(R.id.LoginPasswordField);
 
+        mGoogleBtn= (SignInButton) findViewById(R.id.GoogleBtn);
+
         progressDialog=new ProgressDialog(this);
 
         mAuth=FirebaseAuth.getInstance();
         mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("User");
         mDatabaseUser.keepSynced(true);
+
+
+        //Google Sign Setup
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
+        mGoogleApiClient= new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                Toast.makeText(LoginUser.this, "Connection Failure ....", Toast.LENGTH_SHORT).show();
+            }
+        }).addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+
+        //Google Sign in Button
+        mGoogleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
     }
 
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+// Google sign
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+
+            progressDialog.setMessage("Google Sign in ....");
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                progressDialog.dismiss();
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginUser.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        progressDialog.dismiss();
+                        // ...
+                    }
+                });
+    }
 
     //User Login
-
     public void UserLogin(View view){
 
         String username=editTextUserName.getText().toString().trim();
@@ -53,7 +149,7 @@ public class LoginUser extends AppCompatActivity {
 
         if(!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)){
 
-            progressDialog.setMessage("Signing Up.....");
+            progressDialog.setMessage("Signing in.....");
             progressDialog.show();
             //Login user
 
